609677188141054fac2997dde474edc169901410
Barrett Strausser <j.barrett.strausser@gmail.com>
2013-01-18 18:31:57 -0500

    Fixes actor reference path resolution

--- src/main/scala/rbm/nodes/Synapse.scala
+++ src/main/scala/rbm/nodes/Synapse.scala
@@ -1,4 +1,4 @@
-package barrett.rbm.nodes
+package rbm.nodes
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,11 +8,14 @@ package barrett.rbm.nodes
  * To change this template use File | Settings | File Templates.
  */
 
-import akka.actor._
 import collection.immutable._
+import akka.actor._
 import akka.actor.FSM._
 
+
+
 import rbm.signals._
+import rbm.processing._
 
 trait InSynapseLike
 trait OutSynapseLike
@@ -22,23 +25,38 @@ case object Pending  extends  InSynapseState
 
 
 
-case class InSynapseData(receivedSignals : List[SynapticTransmitter],weights :Map[Int,Float] )
+case class InSynapseData(receivedTransmitters : List[SynapticTransmitter],weights :Map[Int,Double])
 
-class InSynapse(nodeName : String,inputCount : Int) extends Actor with FSM[InSynapseState,InSynapseData]
+class InSynapse(nodeName : String,inputCount : Int, correspondingNeuron :String = "defaultneuron") extends Actor with FSM[InSynapseState,InSynapseData]
 {
 
+  val neuron = correspondingNeuron match {
+
+    case "defaultneuron" => context.actorFor("/user/" + nodeName + "-neuron")
+    case _ => {
+      val l = context.actorFor("/user/" + correspondingNeuron)
+      println("using non default")
+      l
+    }
+
+  }
 
 
+  startWith(Pending,InSynapseData(List.empty[SynapticTransmitter],Map.empty[Int,Double]))
   when(Pending) {
 
     case Event(SynapticTransmitter(b,n),data)  =>  {
 
-      data.receivedSignals.length match {
-        case cnt if cnt == inputCount => {
-            goto(Pending)
+      data.receivedTransmitters.length match {
+        case cnt if cnt + 1 == inputCount  => {
+
+          val joins = for (weight <- data.weights; transmitter <- data.receivedTransmitters; if(weight._1 == transmitter.id)) yield ExcitationWeight(transmitter,weight._2)
+          val signal = ComputeNeuronalSignal.GenerateSignal(joins)
+          neuron !  signal
+          goto(Pending)  using data
 
         }
-        case _ =>  goto(Pending)   using InSynapseData((SynapticTransmitter(b,n) :: data.receivedSignals),data.weights  )
+        case _ =>  goto(Pending)   using InSynapseData((SynapticTransmitter(b,n) :: data.receivedTransmitters),data.weights  )
 
 
 
